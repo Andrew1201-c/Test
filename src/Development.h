99e74dd6ea828fcf3bf5af500f170b04f512c2a3
Andrew0142 <a39294929@gmail.com>
2026-02-06 14:35:40 +0400

    Update Development.h

--- src/Development.h
+++ src/Development.h
@@ -1,10 +1,12 @@
 #ifndef _DEVELOPMENT_H
 #define _DEVELOPMENT_H
 
+/* No longer used */
 namespace Dev {
     bool OnDevelopment() {
-        return true;
+        return false; /* It's originally set as "true," but the well is later changed, because the code is no longer used. */
     }   
 }
+
 
 #endif /* _DEVELOPMENT_H */
